05ba4b8aede665b5927bb5cff03d0245a8fedf1a
ischeez <sekretigor6@gmail.com>
2025-12-21 00:28:23 +0300

    Last change

--- Lesson 3/index.js
+++ Lesson 3/index.js
@@ -1,1 +1,9 @@
-// console.log('fdfsdfds'); gfgdfgfdgdfgdfgdfgfdggdgfd
+const pins = ['1111','5678','1234','9999'];
+
+for (const pin of pins) {
+    if (pin === '1234'){
+        console.log('Пин-код найден');
+        break;
+    }
+    console.log('Неверный Пин-код: ' + pin)
+}
