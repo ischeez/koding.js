a3c6e5588ab6a48ea34ad4bf197c6e385695add8
ischeez <sekretigor6@gmail.com>
2025-12-23 01:21:03 +0300

    Move files into Lesson 4 folder

--- Lesson 3/index.js
+++
@@ -1,9 +0,0 @@
-const pins = ['1111','5678','1234','9999'];
-
-for (const pin of pins) {
-    if (pin === '1234'){
-        console.log('Пин-код найден');
-        break;
-    }
-    console.log('Неверный Пин-код: ' + pin)
-}
