77d7ed2199ab572ae3200195112af9823022fc55
boba <sekretigor6@gmail.com>
2025-12-23 01:16:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -40,36 +40,7 @@ console.log(usdToRub(121));
 // console.log(checkAge(16));
 // console.log(checkAge(21)); 
  /// Альтрнатива 
-=======
-// const age  = 20;
 
-// if (age >= 18) {
-//     console.log('Доступ разрешен');
-// } else {
-//     console.log('Доступ запрещен');
-// }
-
-
-
-
-// let totalPrice = 5000;
-// const accountBalance = 4000;
-// const isVip = true;
-
-
-
-
-// if (isVip===true){
-//     totalPrice = totalPrice * 0.9;               0,9 = 90% ибо 100-10 
-
-
-// } 
-
-// if (accountBalance >= totalPrice){
-//     console.log('Оплата прошла успешно');
-// } else {
-//     console.log('Оплата не успешна');
-// }
 
 
 
